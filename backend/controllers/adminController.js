013a3a79aab0843e568ea854cbf9c208018da0c4
RitJaroen26 <ponpawarit21@gmail.com>
2025-05-01 17:59:40 +0700

    Fix bcrypt issue by replacing with bcryptjs

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,6 +1,6 @@
 import adminModel from "../models/adminModels.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import validator from "validator";
 import userModel from "../models/userModel.js";
 import orderModel from "../models/orderModel.js";
