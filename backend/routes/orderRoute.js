ff1d90cf1e785fe5d9e7c8162de9528d75af3dd8
RitJaroen26 <ponpawarit21@gmail.com>
2025-05-02 21:41:20 +0700

    Updated code

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -7,6 +7,7 @@ const orderRouter = express.Router();
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrders);
+orderRouter.get("/myorders", authMiddleware, userOrders);
 orderRouter.get("/list", listOrders);
 orderRouter.post("/status", updateStatus);
 
